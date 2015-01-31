91e9556400e1f928acb07d918bbedb6801885605
Chaube97 <chaube97@students.byron.k12.mn.us>
2015-01-31 17:50:08 -0600

    Lifter files
    Added values, still need to add SmartDashboard

--- src/org/usfirst/frc/team4859/robot/commands/LiftDown.java
+++ src/org/usfirst/frc/team4859/robot/commands/LiftDown.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4859.robot.commands;
 import org.usfirst.frc.team4859.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LiftDown extends Command
 {
@@ -20,7 +21,7 @@ public class LiftDown extends Command
 	
 	protected void execute()
 	{
-		
+		//SmartDashboard.putNumber("Lift Down Speed", );
 	}
 
 	
--- src/org/usfirst/frc/team4859/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Lifter.java
@@ -1,10 +1,13 @@
 package org.usfirst.frc.team4859.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lifter extends Subsystem
 {
-	//Declare motors here
+	CANTalon motorLifterLeft = new CANTalon(0/*Put CANTalon ID here*/);
+	CANTalon motorLifterRight = new CANTalon(0/*Put CANTalon ID here*/);
+	
 		public Lifter()
 		{
 			super();
@@ -17,17 +20,23 @@ public class Lifter extends Subsystem
 		public void liftUp()
 		{
 			//set motor speeds for when you call LiftUp
+			motorLifterLeft.set(0.25);
+			motorLifterRight.set(0.25);
 		}
 		
 		
 		public void liftDown()
 		{
 			//set motor speeds for when you call LiftDown
+			motorLifterLeft.set(-0.25);
+			motorLifterRight.set(-0.25);
 		}
 		
 		
 		public void liftStop()
 		{
 			//set motor speeds for when you call LiftStop
+			motorLifterLeft.set(0);
+			motorLifterRight.set(0);
 		}
 }
