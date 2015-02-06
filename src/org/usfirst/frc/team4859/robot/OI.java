645c0159cec2e5709efcc18f84096cbc0ec85c3b
Duit <wupy36@gmail.com>
2015-02-06 17:24:41 -0600

    OI Button
    Buttons called

--- src/org/usfirst/frc/team4859/robot/OI.java
+++ src/org/usfirst/frc/team4859/robot/OI.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team4859.robot;
 
+import org.usfirst.frc.team4859.pneumatics.Outp1;
+import org.usfirst.frc.team4859.robot.commands.IntakeIn;
+import org.usfirst.frc.team4859.robot.commands.IntakeOut;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -19,35 +23,20 @@ public class OI {
 	// Define a joystick on port 0
 	Joystick joystickP0 = new Joystick(0);
 	
+	Button precisionMode = new JoystickButton(joystickP0, 8);
+	
 	//created intake in button on joystickP0 on button 1
 	Button intakeIn = new JoystickButton(joystickP0,1);
 	
 	//created intakeOut button on joystickP0 on button 2
 	Button intakeOut = new JoystickButton(joystickP0,2);
 	
-	//// CREATING THINGS
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-	
 	public OI()
 	{
-		joystickP0 = new Joystick(0);
+		precisionMode.toggelwhenPressed(new PrecisionMode());
+		intakeIn.whenPressed(new IntakeIn());
+		intakeOut.whenPressed(new IntakeOut());
+		
 	}
 	
 	
