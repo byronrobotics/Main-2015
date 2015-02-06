2dac3a9ef47281a161a26dda56f14af02f582935
Chaube97 <chaube97@students.byron.k12.mn.us>
2015-02-06 17:06:31 -0600

    minor

--- src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team4859.robot.subsystems;
+
 import org.usfirst.frc.team4859.robot.commands.DriveWithJoystick;
-import org.usfirst.frc.teams4859.robot.ThrottleLookup.ThrottleLookup;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -10,22 +10,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Chassis extends Subsystem
 {
 	//Setting motors to CANTalon IDs
-	static CANTalon motorChassisLeft = new CANTalon(12);
+	static CANTalon motorChassisLeft = new CANTalon(13);
 	static CANTalon motorChassisRight = new CANTalon(14);
 	
 	//front is top of "U" back is bottom of "U"
-<<<<<<< HEAD
-	static RobotDrive drive = new RobotDrive(motorLeft, motorRight);
-	
-=======
 	static RobotDrive drive = new RobotDrive(motorChassisLeft, motorChassisRight);
->>>>>>> origin/master
 
 	
 	public Chassis()
 	{
 		super();
-
+		
 		//Set a timeout for the motors (1 second)
 		drive.setSafetyEnabled(true);
 		drive.setExpiration(1);
@@ -73,38 +68,21 @@ public class Chassis extends Subsystem
 	
 	public void driveWithJoystick(Joystick joystickP0){
 		//calling the deadzone a double, done to change value in dashboard
-<<<<<<< HEAD
-		double dz = SmartDashboard.getNumber("Deadzone");
-		SmartDashboard.putNumber("Deadzone_Manual", dz);
-		final double deadzone = dz;
-=======
 		double deadzone = SmartDashboard.getNumber("Deadzone");
 		SmartDashboard.putNumber("Deadzone Manual", deadzone);
->>>>>>> origin/master
 		
-		//final double deadzone = .015; // amount of deadspace around center
-		//double x;
 		double y;
 		double twist;
 		
 		// Get simple values from joystick
-		//x = joystickP0.getX();
-		y = joystickP0.getY();
 		twist = joystickP0.getTwist(); //z-axis check joysticks
 		
 		// Get values from joystick, square, and apply deadzone
-		//x = joystickAdjust(joystickP0.getX(), deadzone);
-		
-		SmartDashboard.putNumber("Orig Y", y);
-		y = ThrottleLookup.calcJoystickCorrection(y);
-		SmartDashboard.putNumber("After Y", y);
-
-		
 		y = joystickAdjust(joystickP0.getY(), deadzone);
-		//twist = joystickAdjust(joystickP0.getTwist(), deadzone);
-	
-		twist = twist/1.75;
-		y = -y/1.25;
+
+		// Adjusting for variables
+		twist = twist/1.25;
+		y = -y;
 		
 		
 		//m012 is CANTalon 12 encoder 
@@ -126,10 +104,13 @@ public class Chassis extends Subsystem
 		//here we are stating that we have to use button 3 in order to use encoders to set the speed of the wheels
 		//BE CAREFUL USING BUTTON 3!!
 		//it swaps between our joystick drive and our encoder drive
-		if(joystickP0.getRawButton(3)){
+		if(joystickP0.getRawButton(3))
+		{
 			motorChassisLeft.set(m012);
 			motorChassisRight.set(m014);
-		} else {
+		}
+		else
+		{
 			drive.arcadeDrive(twist, y);
 		}
 	}
