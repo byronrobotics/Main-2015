ec9801cae1d4ec1616a81f6cd7c4a0b0c9648d45
Duit <wupy36@gmail.com>
2015-02-14 14:37:30 -0600

    PhotoSwitch =Auto
    Auto classes created and then some

--- src/org/usfirst/frc/team4859/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Lifter.java
@@ -13,7 +13,7 @@ public class Lifter extends Subsystem
 {	
 	static CANTalon motorLifter = new CANTalon(RobotMap.talonDevIDLifter);
 	static DigitalSource limitSwitch = new DigitalInput(16);
-
+	static DigitalInput photoSwitch = new DigitalInput(1);
 		
 		public Lifter(){
 			super();
@@ -25,8 +25,7 @@ public class Lifter extends Subsystem
 		
 		public void liftUp(){ // brings lift up du
 			//set motor speeds for when you call LiftUp
-			
-			motorLifter.set(0.50);
+			motorLifter.set(-1.0);
 			SmartDashboard.putNumber("LiftMotor Right", motorLifter.getEncVelocity());
 			SmartDashboard.putBoolean("LiftDown", false);
 			SmartDashboard.putBoolean("LiftUp", true);
@@ -36,18 +35,33 @@ public class Lifter extends Subsystem
 		public void liftDown() //brings lift down du.
 		{
 			//set motor speeds for when you call LiftDown
-			motorLifter.set(-1.0);
-			SmartDashboard.putNumber("LiftMotor Right", motorLifter.getEncVelocity());
-			SmartDashboard.putBoolean("LiftDown", true);
-			SmartDashboard.putBoolean("LiftUp", false);
+				motorLifter.set(0.5);
+				SmartDashboard.putNumber("LiftMotor Right", motorLifter.getEncVelocity());
+				SmartDashboard.putBoolean("LiftDown", true);
+				SmartDashboard.putBoolean("LiftUp", false);	
 		}
 		
 		
 		public void liftStop() //stops lift motors du
 		{
 			//set motor speeds for when you call LiftStop
-			motorLifter.set(0);			SmartDashboard.putNumber("LiftMotor", motorLifter.getEncVelocity());
+			motorLifter.set(0);			
+			SmartDashboard.putNumber("LiftMotor", motorLifter.getEncVelocity());
 			SmartDashboard.putBoolean("LiftDown", false);
 			SmartDashboard.putBoolean("LiftUp", false);
+		}
+		public void liftauto()
+		{
+			//set motor speeds for when you call LiftUp
+			if(photoSwitch.get() ==true){	
+					motorLifter.set(-1.0);
+					SmartDashboard.putNumber("LiftMotor Right", motorLifter.getEncVelocity());
+					SmartDashboard.putBoolean("LiftDown", false);
+					SmartDashboard.putBoolean("LiftUp", true);
+				} else if(photoSwitch.get() ==false){
+					motorLifter.set(0.0);
+					SmartDashboard.putString("LiftDown", "error-not-in");
+					SmartDashboard.putString("LiftUp", "error-not-in");
+				} 
 		}
 }
