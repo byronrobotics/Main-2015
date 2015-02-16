3154855f5c2b85cc2f4536b9a04c09550730e27c
daheinz <heinz.david@gmail.com>
2015-02-15 22:13:04 -0600

    Minor Fix

--- src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
@@ -67,7 +67,6 @@ public class Chassis extends Subsystem
 
 		// Adjusting for variables
 		twist = -twist/1.25;
-		y = y;
 		
 		SmartDashboard.putBoolean("Precise", RobotMap.pMode);
 		//m012 is CANTalon 12 encoder 
