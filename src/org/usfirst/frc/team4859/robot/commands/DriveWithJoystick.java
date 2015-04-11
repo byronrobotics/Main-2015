9362831a92f3f7213406ed5816801274a23f21c9
Chaube97 <chaube97@students.byron.k12.mn.us>
2015-04-11 17:25:09 -0500

    Re did some comments that were removed at the merge

--- src/org/usfirst/frc/team4859/robot/commands/DriveWithJoystick.java
+++ src/org/usfirst/frc/team4859/robot/commands/DriveWithJoystick.java
@@ -1,35 +1,35 @@
 package org.usfirst.frc.team4859.robot.commands;
 
 import org.usfirst.frc.team4859.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-public class DriveWithJoystick extends Command
-{
+public class DriveWithJoystick extends Command {
 
-	public DriveWithJoystick()
+	public DriveWithJoystick() // Takes control of the Chassis subsystem so no other command runs on it
 	{
 		requires(Robot.chassis);
 	}
 	
-	protected void initialize(){}
+	protected void initialize() // Any code that is run at the beginning of the command should be placed here
+	{
+	}
 	
-	protected void execute()
+	protected void execute() // Runs the drive method in Chassis with the joystick values every 20ms/50 times per second
 	{
 		Robot.chassis.driveWithJoystick(Robot.oi.getJoystick());
 	}
 	
-	protected boolean isFinished()
+	protected boolean isFinished() // Checks if the command has finished running
 	{
 		return false;
 	}
 	
-	protected void end()
+	protected void end() // If isFinished returns true, run this method, which should stop what the command was doing
 	{
-		//Robot.chassis.driveWithJoystick(0,0);
 	}
 	
-	protected void interrupted()
+	protected void interrupted() // Runs if a command takes control of the subsystem this command was using
 	{
+		end();
 	}	
 }
--- src/org/usfirst/frc/team4859/robot/commands/PrecisionOff.java
+++ src/org/usfirst/frc/team4859/robot/commands/PrecisionOff.java
@@ -1,38 +1,37 @@
 package org.usfirst.frc.team4859.robot.commands;
 
 import org.usfirst.frc.team4859.robot.RobotMap;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class PrecisionOff extends Command {
 
-    public PrecisionOff() {    	
-   }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	System.out.println("Precision Off");
-    	RobotMap.pMode = false;
-     }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    public PrecisionOff()
+    {
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected void initialize() // Called just before this command runs the first time
+    {
+    	System.out.println("Precision Off");
+    	RobotMap.pMode = false;
+    }
+
+    protected void execute() // Called repeatedly when this command is scheduled to run
+    {
+    }
+
+    protected boolean isFinished() // Make this return true when this command no longer needs to run execute()
+    {
         return true;
     }
 
-    // Called once after isFinished returns true
-    protected void end() {
+    protected void end() // Called once after isFinished returns true
+    {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted()
+    {
+    	end();
     }
 }
