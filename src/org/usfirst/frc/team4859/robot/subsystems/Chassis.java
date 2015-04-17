f0ae7c42c42d58e533e2bbcbdcb553dd7eec9181
Chaube97 <chaube97@students.byron.k12.mn.us>
2015-04-17 18:15:23 -0500

    noo

--- src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
@@ -50,8 +50,6 @@ public class Chassis extends Subsystem
 		SmartDashboard.putNumber("JoystickY", yAxis);
 		SmartDashboard.putNumber("JoystickTwist", twist);
 		SmartDashboard.putBoolean("Precision Mode", RobotMap.pMode);
-		
-		chassisDrive.arcadeDrive(yAxis, twist);
 	}
 	
 	public void DriveStraight(){		
