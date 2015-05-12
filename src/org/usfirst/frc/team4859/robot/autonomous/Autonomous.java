42b9a62e00a61a94a53edfc1c3ed2966ece4afb8
Andrew <wupy36@gmail.com>
2015-05-12 16:24:04 -0500

    Auto
    tested

--- src/org/usfirst/frc/team4859/robot/autonomous/Autonomous.java
+++ src/org/usfirst/frc/team4859/robot/autonomous/Autonomous.java
@@ -22,6 +22,19 @@ public class Autonomous extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	addSequential(new DriveStopTime(0.5));
+    	addParallel(new LiftUpTime(0.3));
+    	addSequential(new DriveStraight(1.25));
+    	addSequential(new LiftDownTime(0.25));
+    	addParallel(new LiftUpTime(0.30));
+    	addSequential(new DriveStraight(1.3));
+    	addSequential(new LiftDownTime(0.2));
+    	addParallel(new LiftUpTime(0.35));
+    	addSequential(new DriveRight180(0.50));
+    	addSequential(new DriveStraight(1.5));
+    	addParallel(new LiftDownTime(0.20));
+    	addSequential(new DriveLeft180(0.50));
+    	addSequential(new DriveBackwards(0.5));
     	addSequential(new DriveStop());
     }
 }
