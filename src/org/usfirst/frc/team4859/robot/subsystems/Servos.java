4002d83f9177b76ec16834edcfdae634cd05b25e
byronrobotics <byronrobotics@gmail.com>
2015-11-07 16:34:32 -0600

    fds

--- src/org/usfirst/frc/team4859/robot/subsystems/Servos.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Servos.java
@@ -1,27 +1,21 @@
 package org.usfirst.frc.team4859.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PWM.PeriodMultiplier;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Servos extends Subsystem {
-    
-	private Servo binServo = new Servo(9);
-	
-	
-	
-    public void initDefaultCommand()
-    {
-    	binServo.setPeriodMultiplier(PeriodMultiplier.k4X);
-    }
 
-	public void servo0()
-	{
-		binServo.set(0.0);	
+	private Servo binServo = new Servo(9);
+
+	public void initDefaultCommand() {
+		// binServo.setBounds(1.9, 0, 0, 0, 1.5);
 	}
 
-	public void servo90()
-	{
-		binServo.set(1.0);
+	public void servo0() {
+		binServo.setAngle(180.0);
+	}
+
+	public void servo90() {
+		binServo.setAngle(270.0);
 	}
 }
